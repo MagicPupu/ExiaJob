fda520998122f3954861387e218d952e3068a536
MagicPupu <83464334+MagicPupu@users.noreply.github.com>
2023-03-21 16:30:36 +0100

    changing div names in student page

--- exiajob/resources/views/pages/students.blade.php
+++ exiajob/resources/views/pages/students.blade.php
@@ -5,28 +5,28 @@
 @section('content')
 <body>
   <main>
-    <h1 id="entreprises-h1">Etudiants</h1>
-    <div class="search-bar-entreprise">
+    <h1 id="etudiants-h1">Etudiants</h1>
+    <div class="search-bar-etudiant">
       <div class="blue-search-icon">
         <i id="blue-circle"></i>
-        <span class="search-icon-entreprise"><i class="fas fa-search"></i></span>
+        <span class="search-icon-etudiant"><i class="fas fa-search"></i></span>
       </div>
       <button class="filtrer" type="button">
         Filter par
       </button>
-      <input id="search-entreprise" type="text" placeholder="Chercher un Pilote, un centre, ...">
+      <input id="search-etudiant" type="text" placeholder="Chercher un Pilote, un centre, ...">
     </div>
-    <p id="sous-entreprises">Penasaran apa saja review dari pengguna yang memakai aplikasi dan website Destinize buat
+    <p id="sous-etudiants">Penasaran apa saja review dari pengguna yang memakai aplikasi dan website Destinize buat
       <br> nentuin kemana liburan mereka selanjutnya? Yuk cek dibawah!</p>
     <p id="studentNumber">{{ $students->count() }} etudiants</p>
 
     <!--box entreprise-->
-    <div class="box-entreprise">
+    <div class="box-etudiant">
       @foreach($students as $student)
-      <div class="container-entreprise">
-        <div class="card-entreprise">
-          <div class="front-entreprise">
-            <div class="front-content-entreprise">
+      <div class="container-etudiant">
+        <div class="card-etudiant">
+          <div class="front-etudiant">
+            <div class="front-content-etudiant">
               <img src="{{ asset('/images/profile_pictures/'.$student->image) }}" alt="photo" />
               <h1>{{ $student->name }}</h1>
               <p>{{ $student->description }}</p>
